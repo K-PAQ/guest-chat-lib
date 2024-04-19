2f57b34199b0a3b7b8b7d7c2f63011e5601e83bc
Frank2899 <aughusf089@gmail.com>
2024-04-20 05:40:41 +0800

    Docs: Added Theme

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,12 @@
 import { addons } from '@storybook/manager-api'
+import { create } from '@storybook/theming/create'
 
 addons.setConfig({
+    theme: create({
+        base: 'dark',
+        brandTitle: 'Guest Chat Lib',
+        brandUrl: '#'
+    }),
     rightPanelWidth: 800,
     panelPosition: 'right',
     initialActive: 'sidebar',
